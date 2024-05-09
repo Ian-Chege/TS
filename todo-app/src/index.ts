388ef4e7ea492d7fb7a404ecf8251bf03b3c6c92
Ian Chege <en20101832020@student.mut.ac.ke>
2024-05-09 09:06:13 +0300

    using JS map

--- todo-app/src/index.ts
+++ todo-app/src/index.ts
@@ -1,2 +1,18 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+
+let todos: TodoItem[] = [
+  new TodoItem(1, "Buy Flowers"),
+  new TodoItem(2, "Get Shoes"),
+  new TodoItem(3, "Collect Tickets"),
+  new TodoItem(4, "Call Joe", true),
+];
+
+let collection: TodoCollection = new TodoCollection("Ian", todos);
 console.clear();
-console.log("User's Todo list");
+console.log(`${collection.userName}'s Todo List`);
+
+let newId: number = collection.addTodo("Go for run");
+let todoItem: TodoItem = collection.getTodoById(newId);
+todoItem.printDetails();
+// collection.addTodo(todoItem);
